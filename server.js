3c2739950e759852d4c45b747e502fb10cbaeb3e
Jorge Echeverria <coko.eche@gmail.com>
2023-04-18 16:02:10 +1000

    link database with /image endpoint

--- server.js
+++ server.js
@@ -83,19 +83,15 @@ app.get("/profile/:id", (req, res) => {
     });
 });
 
+//updates the entries and increases the count
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("not found");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => res.json(entries[0].entries))
+    .catch((err) => res.status(400).json("unable to get entries"));
 });
 
 app.listen(3000, () => {
