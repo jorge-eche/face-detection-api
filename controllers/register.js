638695b9b119ad0a31e8e46e4175f8aeff70fcd1
Jorge Echeverria <coko.eche@gmail.com>
2023-05-05 10:45:16 +1000

    fix bug with register

--- controllers/register.js
+++ controllers/register.js
@@ -19,17 +19,17 @@ export const handleRegister = (req, res, db, bcrypt) => {
       .into("login")
       .returning("email")
       .then((loginEmail) => {
-        trx("users")
-          .returning("*")
-          .insert({
-            name: name,
-            email: loginEmail[0].email,
-            joined: new Date(),
-          })
-          .then((user) => res.json(user[0]));
+        return trx("users").returning("*").insert({
+          name: name,
+          email: loginEmail[0].email,
+          joined: new Date(),
+        });
+        // .then((user) => res.json(user[0]));
       })
-      .then(() => trx.commit)
-      .catch(() => trx.rollback)
-      .catch((err) => res.status(400).json(err));
-  }).catch((err) => res.status(400).json(err));
+      .then((user) => {
+        trx.commit();
+        res.json(user[0]);
+      })
+      .catch(() => trx.rollback());
+  }).catch((err) => res.status(400).json("unable to register"));
 };
